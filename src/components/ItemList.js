0b57648ae49f12bfcc609dd02628d6cc8341e1f3
ramiporta <p0rt4csd@gmail.com>
2021-12-19 18:28:20 -0300

    Correccion desafio catalogo (clase 6)

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,24 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Item from './Item';
 
-const ItemList = () => {
-    const [productos, setProductos] = useState([])
-    /* Async mock (simulacion de proceso asincronico usando promise) */
-    const API = new Promise((resolve, reject) => {
-        setTimeout(()=>{
-        resolve(
-            [{id: 1, title: "Remera", description: "Tremenda remera que va con vos", price: 2000, pictureUrl: "logo.png", stock: 4},
-            {id: 2, title: "Remera2", description: "Tremenda remera que va con vos", price: 3000, pictureUrl: "logo192.png", stock: 0},
-            {id: 3, title: "Remera3", description: "Tremenda remera que va con vos", price: 4000, pictureUrl: "logo512.png", stock:1},
-            {id: 4, title: "Remera4", description: "Tremenda remera que va con vos", price: 1000, pictureUrl: "logo.png", stock:2}
-            ]
-        )
-        }, 2000)
-    })
-    API.then((res)=>{
-        setProductos(res)
-    })
-
+const ItemList = ({items}) => {
+    const productos = items
     return (
         <div className="row">
             {
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import ItemList from './ItemList';
 
 export default function ItemListContainer (props) {
     const Greeting = props.greeting;
+    const [productos, setProductos] = useState([])
+    useEffect(() => {
+        /* Async mock (simulacion de proceso asincronico usando promise) */
+        const API = new Promise((resolve, reject) => {
+            setTimeout(()=>{
+            resolve(
+                [{id: 1, title: "Remera", description: "Tremenda remera que va con vos", price: 2000, pictureUrl: "logo.png", stock: 4},
+                {id: 2, title: "Remera2", description: "Tremenda remera que va con vos", price: 3000, pictureUrl: "logo192.png", stock: 0},
+                {id: 3, title: "Remera3", description: "Tremenda remera que va con vos", price: 4000, pictureUrl: "logo512.png", stock:1},
+                {id: 4, title: "Remera4", description: "Tremenda remera que va con vos", price: 1000, pictureUrl: "logo.png", stock:2}
+                ]
+            )
+            }, 2000)
+        })
+        API.then((res)=>{
+            setProductos(res)
+        })
+    }, [])
+    
     return (
         <div className="container">
             <h2 className="mt-5 pt-5 mb-5 text-center"> {Greeting} </h2>
-            <ItemList></ItemList>
+            <ItemList items={productos}></ItemList>
         </div>
     )
 }
