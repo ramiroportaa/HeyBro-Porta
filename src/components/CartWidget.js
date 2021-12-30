8da3b12a1517386fdfa66ed1f16a1df6c3bb28bd
ramiporta <p0rt4csd@gmail.com>
2021-12-29 23:28:12 -0300

    Contador del CartWidget

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { CartContext } from '../context/cartContext';
 
 export default function CartWidget () {
+
+    const useCart = React.useContext(CartContext);
+    let cartSize = useCart.cart.items.length
     return (
-         <i id="nav-carrito" className="fas fa-shopping-cart"><span className="ms-1">Carrito</span></i>   
+         <i id="nav-carrito" className="fas fa-shopping-cart"><span className="ms-1">Carrito ({cartSize})</span></i>   
     )
 }
