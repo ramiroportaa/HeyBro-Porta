6c3d2227daefc9059be74de59f433bd81175acbe
ramiporta <p0rt4csd@gmail.com>
2022-01-18 01:40:36 -0300

    Formulario de contacto

--- src/components/FormContacto/index.js
+++ src/components/FormContacto/index.js
@@ -1,37 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./FormContacto.css"
 
 const FormContacto = () => {
+    //Regex de validacion formulario.
+    const nombreRegex = /^[a-zA-Z+ ?]{3,16}$/
+    const emailRegex = /^[a-zA-Z0-9\._-]+@{1}[a-zA-Z0-9-]{2,30}[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$/
+    const telRegex = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{7,8}$/
+
+    //Hooks states para validaciones del formulario.
+    const [nombre, setNombre] = useState()
+    const [apellido, setApellido] = useState()
+    const [email, setEmail] = useState()
+    const [telefono, setTelefono] = useState()
+
+    //Functions para validar cada input del formulario.
+    const valorNombre=(value)=>{
+        nombreRegex.test(value) ? setNombre(value) : setNombre();
+    }
+
+    const valorApellido=(value)=>{
+        nombreRegex.test(value) ? setApellido(value) : setApellido();
+    }
+
+    const valorEmail=(value)=>{
+        emailRegex.test(value) ? setEmail(value) : setEmail();
+    }
+
+    const valorTel=(value)=>{
+        telRegex.test(value) ? setTelefono(value) : setTelefono();
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        if (nombre && apellido && email && telefono){
+            alert("MENSAJE ENVIADO")
+            e.target.reset()
+            setNombre()
+            setApellido()
+            setEmail()
+            setTelefono()
+        }else{
+            alert("ERROR, Revise campos en rojo")
+        }
+    }
+
     return (
         <>
-        <form className='mb-5'>
+        <form className='mb-5' onSubmit={handleSubmit}>
             <fieldset>
                 <legend className="text-center">Formulario de Contacto</legend>
 
                 <div className="form-group row mb-1">
                     <span className="col-1 text-center"><i className="fa fa-user bigicon"></i></span>
                     <div className="col-11">
-                        <input id="fname" name="name" type="text" placeholder="Nombre" className="form-control iContacto"/>
+                        <input id="fname" name="name" type="text" placeholder="Nombre" className={`form-control iContacto ${nombre ? "border border-success" : "border border-danger"}`} onChange={(e)=>valorNombre(e.target.value)}/>
                     </div>
                 </div>
                 <div className="form-group row mb-1">
                     <span className="col-1  text-center"><i className="fa fa-user bigicon"></i></span>
                     <div className="col-11">
-                        <input id="lname" name="name" type="text" placeholder="Apellido" className="form-control iContacto"/>
+                        <input id="lname" name="name" type="text" placeholder="Apellido" className={`form-control iContacto ${apellido ? "border border-success" : "border border-danger"}`} onChange={(e)=>valorApellido(e.target.value)}/>
                     </div>
                 </div>
 
                 <div className="form-group row mb-1">
                     <span className="col-1  text-center"><i className="fa fa-envelope bigicon"></i></span>
                     <div className="col-11">
-                        <input id="email" name="email" type="email" placeholder="Email" className="form-control iContacto"/>
+                        <input id="email" name="email" type="email" placeholder="Email" className={`form-control iContacto ${email ? "border border-success" : "border border-danger"}`} onChange={(e)=>valorEmail(e.target.value)}/>
                     </div>
                 </div>
 
                 <div className="form-group row mb-1">
                     <span className="col-1  text-center"><i className="fab fa-whatsapp-square bigicon"></i></span>
                     <div className="col-11">
-                        <input id="phone" name="phone" type="tel" placeholder="WhatsApp" className="form-control iContacto"/>
+                        <input id="phone" name="phone" type="tel" placeholder="WhatsApp" className={`form-control iContacto ${telefono ? "border border-success" : "border border-danger"}`} onChange={(e)=>valorTel(e.target.value)}/>
                     </div>
                 </div>
 
