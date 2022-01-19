052c40cfdedbbc321de266a1820d94415d6cf68a
ramiporta <p0rt4csd@gmail.com>
2022-01-19 15:47:33 -0300

    frases dinamicas

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import "./Home.css";
 
 const Home = () => {
+  const [frase, setFrase] = useState("X")
+  const [pos, setPos] = useState(0)
+
+  useEffect(() => {
+    const frases = ["COMIENZA A COMPRAR AHORA", "SOMOS TU MEJOR OPCION", `10%OFF CON EL CODIGO "CODERHOUSE"`]
+    setTimeout(() => {
+      setFrase(frases[pos])
+      pos < frases.length-1 ? setPos(pos+1) : setPos(0);
+    }, 5000);
+
+  }, [frase, pos]);
+  
+
     return (
         <>
             <div id="portada" className="bg-image" Style="background-image: url(/portada.jpg); width: 100vw; height: 100vh;">
@@ -11,7 +24,7 @@ const Home = () => {
                   <div className="col-lg-6">
                     <div Style="min-height: 22rem;">
                       <h2 className="portada-text mt-5">
-                        COMIENZA A COMPRAR AHORA!
+                          {frase}
                       </h2>
                     </div>
                     <Link to="/productos" className="btn portada-boton" >IR A LA TIENDA</Link>
